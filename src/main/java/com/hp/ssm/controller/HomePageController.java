3ae380657705e49dfe10637a08e6007318083793
xiaosmile <a1226165225@outlook.com>
2019-04-07 13:19:36 +0800

    remove test

--- src/main/java/com/hp/ssm/controller/HomePageController.java
+++ src/main/java/com/hp/ssm/controller/HomePageController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomePageController {
     @GetMapping("/home")
     public String getHomePage(){
-        Integer i = null;
-        int j = i;
         return "homePage";
     }
 }
