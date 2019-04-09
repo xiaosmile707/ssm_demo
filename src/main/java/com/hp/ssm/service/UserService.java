2462c473182068ab1938ded0a4164799fe930fb1
xiaosmile <a1226165225@outlook.com>
2019-04-09 14:59:36 +0800

    update header

--- src/main/java/com/hp/ssm/service/UserService.java
+++ src/main/java/com/hp/ssm/service/UserService.java
@@ -4,19 +4,32 @@ import com.hp.ssm.model.User;
 
 public interface UserService {
     void addUser(User user);
+
     long getExceptionUserByEmail(String email);
+
     void setUserActive(int id);
+
     boolean checkAccount(User user);
+
     User getUserByEmail(String email);
+
     User getUserById(int id);
+
     String getSecretByEmail(String email);
-    void updateSecretByEmail(String email,String secret);
+
+    void updateSecretByEmail(String email, String secret);
+
     void setTwoStepsActive(String email);
+
     void setTwoStepsUnActive(String email);
+
     void updateUser(User user);
+
     boolean checkTwoStepsActiveByEmail(String email);
-    void userRealNameAuth(int userId,String idNumber);
-    void addUserPic(int userId,String userPic);
+
+    void userRealNameAuth(int userId, String idNumber);
+
+    void addUserPic(int userId, String userPic);
 
     String getUserPic(Integer userId);
 
